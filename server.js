ee1a9c223fb3c6ca61f6f3080cf8e74f550aef16
phamnhatquangdepzai <phamnhatquangdepzai@gmail.com>
2026-05-20 19:52:57 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  res.end("Pham Quang Server Online");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running");
+});
